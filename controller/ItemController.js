0c5746976425f197e509b38c1d278e1e5d680794
chengyurou <18829292695@163.com>
2017-01-11 14:07:22 +0800

    finish item

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -1,5 +1,57 @@
+import Item from '../models/item';
+
+
 export default class ItemController {
-    getAll(req, res, next) {
-        res.send("should return all items");
-    }
+  getAll(req, res, next) {
+    Item.find({}, (err, doc)=> {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send(doc);
+      }
+    });
+  }
+
+  createItem(req, res, next) {
+    Item.create(req.body, (err, doc) => {
+      if (err) {
+        throw err;
+      } else {
+        res.sendStatus(201);
+      }
+    });
+  }
+
+  getOneItem(req, res, next) {
+    const id = req.params.id;
+    Item.findOne({_id: id}, (err, doc) => {
+      if (err) {
+        throw err;
+      } else {
+        res.send(doc);
+      }
+    })
+  }
+
+  updateItem(req, res, next) {
+    const id = req.params.id;
+    Item.update({_id: id}, {$set: req.body}, (err, doc) => {
+      if (err) {
+        throw err;
+      } else {
+        res.send(204);
+      }
+    });
+  }
+
+  deleteItem(req, res, next) {
+    const id = req.params.id;
+    Item.remove({_id: id}, (err, doc) => {
+      if (err) {
+        throw err;
+      } else {
+        res.send(204);
+      }
+    })
+  }
 }
--- router/routers/items.js
+++ router/routers/items.js
@@ -1,10 +1,13 @@
 import {Router} from 'express';
 import ItemController from '../../controller/ItemController';
 
-
 const router = Router();
 const itemCtrl = new ItemController();
 
 router.get('/', itemCtrl.getAll);
+router.get('/:id',itemCtrl.getOneItem);
+router.post('/',itemCtrl.createItem);
+router.delete('/:id',itemCtrl.deleteItem);
+router.put('/:id',itemCtrl.updateItem);
 
 export default router;
